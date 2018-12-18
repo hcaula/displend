00543eb04a4221942940630eedee7f065eb015e7
Henrique Caúla <lhtc@cin.ufpe.br>
2018-12-18 04:12:31 -0300

    Adds clock

---
+++ Assets/Scripts/ClockController.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class ClockController : MonoBehaviour {
+	
+	public Text clock;
+	void Update () {
+		clock.text = DateTime.Now.ToString("hh:mm tt");
+	}
+}
